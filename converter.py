9d9a29878b8b54e026d476f81affd43b0bde5bf7
Pavel Lankmiler <pavel@incorporatenow.com>
2021-06-02 18:07:59 +0300

    First tests

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import re
 
 try:
     from bs4 import BeautifulSoup
@@ -14,6 +15,8 @@ parser.add_argument('--layout', metavar='layout', type=str, help='layout file')
 parser.add_argument('--contentclass', metavar='contectclass', type=str, help='Content class of child tags')
 parser.add_argument('--contentid', metavar='contectid', type=str, help='Content id of child tags')
 parser.add_argument('--output_dir', metavar='output_dir', type=str, help='output directory to save files')
+parser.add_argument('--extra', metavar='extra', type=str, help='Extra content to be inserted to layout')
+
 
 layout = None
 
@@ -23,17 +26,77 @@ files = all_args.files.split(',')
 
 for item in files:
 	with open(item, 'r') as html_file:
-		soup = BeautifulSoup(html_file.read(), 'html.parser')
+		all_content = BeautifulSoup(html_file.read(), 'html.parser')
 		if all_args.contentid:
-			content = soup.find("div", {"id": all_args.contentclass})
+			content = all_content.find("div", {"id": all_args.contentclass})
 			divsoup = BeautifulSoup(str(content), 'html.parser')
-			print(divsoup.prettify())
+			for elm in content.find_next_siblings():
+			    elm.extract()
+			content.extract()
+
+			content = '''
+@extends('layouts.front')
+
+@section('title', '{}')
+
+@section('content')
+'''.format(title) + content.prettify() + ''' @endsection'''
+			f = open('./output/' + item.split('.')[0] + '.blade.php', 'a')
+			f.write(content)
+			f.close()
+			
+
+			if not layout:
+				layout = all_content
+				body = layout.find('body')
+				body.append("@yield('content')")
+				layout_str = layout.prettify()
+			
+				if all_args.extra:
+					pos = layout_str.find('</body>')
+					new_layout = layout_str[:pos] + all_args.extra + layout_str[pos:]
+					layout = new_layout
+
+					f = open("layout.blade", "a")
+					f.write(str(layout))
+					f.close()
+
 			sys.exit(0)
 		elif all_args.contentclass:
-			content = soup.find("div", {"class": all_args.contentclass})
+			title = all_content.find('title').text
+			content = all_content.find("div", {"class": all_args.contentclass})
 			divsoup = BeautifulSoup(str(content), 'html.parser')
-			print(divsoup.prettify())
-			sys.exit(0)
+			for elm in content.find_next_siblings():
+			    elm.extract()
+			content.extract()
+
+			content = '''
+@extends('layouts.front')
+
+@section('title', '{}')
+
+@section('content')
+'''.format(title) + content.prettify() + ''' @endsection'''
+			f = open('./output/' + item.split('.')[0] + '.blade.php', 'a')
+			f.write(content)
+			f.close()			
+
+			if not layout:
+				layout = all_content
+				body = layout.find('body')
+				body.append("@yield('content')")
+				layout_str = layout.prettify()
+			
+				if all_args.extra:
+					pos = layout_str.find('</body>')
+					new_layout = layout_str[:pos] + all_args.extra + layout_str[pos:]
+					layout = new_layout
+
+					f = open("./output/layout.blade.php", "a")
+					f.write(str(layout))
+					f.close()
+				#print(str(body.encode(formatter='html5')))
+			#sys.exit(0)
 		else:
 			print('No selector given!')
 			sys.exit(0)
